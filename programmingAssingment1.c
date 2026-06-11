c7e60b74ac9f10dd0cf631a7ee58e0fa00aa1d4e
devMrAshish <mrashish1795@gmail.com>
2026-06-11 12:22:47 +0530

    Create programmingAssingment1.c

---
+++ programmingAssingment1.c
@@ -0,0 +1,13 @@
+// This is a program to convert temperature given in celsius to farenheit
+# include <stdio.h>
+int main(){
+
+float celsius; 
+
+scanf("%f",&celsius);
+
+printf("%f",(celsius*(9.0/5)+32));
+
+return 0;
+
+}
